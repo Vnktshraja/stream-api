4ce8dccb1615b257c2401d8c41d47ca1db5a8810
Venkatesh Raja <94715141+Vnktshraja@users.noreply.github.com>
2023-12-01 20:18:49 +0530

    java interview methods added Version 2

--- src/main/java/com/coding/stream/api/Demo.java
+++ src/main/java/com/coding/stream/api/Demo.java
@@ -1,22 +1,47 @@
 package com.coding.stream.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 @Slf4j
 public class Demo {
 
     public static void main(String[] args) {
         findDuplicateCharInMyName();
         compareTwoArrays();
+        storeUniqueDataInMap();
+        convertListOfObjectIntMap();
+    }
+
+    private static void convertListOfObjectIntMap() {
+        Store s1 = new Store(1,"a","chennai");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String,Object> storeMap = objectMapper.convertValue(s1, Map.class);
+        log.info("List of Object converted into Map" + storeMap);
+    }
+
+
+    private static void storeUniqueDataInMap() {
+        Store s1 = new Store(1,"a","chennai");
+        Store s2 = new Store(2,"b","Bangalore");
+        Store s3 = new Store(1,"c","Kolkata");
+        Store s4 = new Store(4,"d","Mumbai");
+        Store s5 = new Store(1,"e","Delhi");
+        LinkedHashMap<Integer,Object> stores = new LinkedHashMap<>();
+        stores.put(s1.id,s1);
+        stores.put(s2.id,s2);
+        stores.put(s3.id,s3);
+        stores.put(s4.id,s4);
+        stores.put(s5.id,s5); // in map if we have duplicate key in map it is printing the last object
+        log.info("Storing"+'\n'+ stores);
     }
 
     private static void compareTwoArrays() {
         int[] arr1 = {1,2,3,4,5,6};
         int[] arr2 = {7,8,9,2,1,0};
+        int count = 0;
         List<Integer> list = new ArrayList<>();
 
         for (int i = 0 ; i<arr1.length ; i++){
@@ -24,10 +49,12 @@ public class Demo {
                 if (arr1[i] == arr2[j]) {
                     log.info("same vales are " + arr1[i] );
                     list.add(arr1[i]);
+                    count++;
                 }
             }
         }
         log.info("duplicate values : " + list);
+        log.info("No of char repeated : " + count);
     }
 
     private static void findDuplicateCharInMyName() {
@@ -42,6 +69,54 @@ public class Demo {
             log.info("No Duplicate character in you name");
         }else {
             log.info("Duplicate found");
+        }
+    }
+
+    public static class Store{
+        int id;
+        String name;
+        String location;
+
+        public Store() {
+        }
+
+        public Store(int id, String name, String location) {
+            this.id = id;
+            this.name = name;
+            this.location = location;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+        @Override
+        public String toString() {
+            return "Store{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", location='" + location + '\'' +
+                    '}';
         }
     }
 }
